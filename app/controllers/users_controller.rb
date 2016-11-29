444809cf7a03ef3e8b99268e617e5ae008f00f1a
congago <tnngocthinh4@gmail.com>
2016-11-29 10:37:12 +0700

    finish chapter7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
-    debugger
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
@@ -19,8 +18,12 @@ class UsersController < ApplicationController
     end
   end
 
-  def user_params
+  
+  private
+
+    def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
 end
