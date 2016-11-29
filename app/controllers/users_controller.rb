596a7f0c3bb726c11e63052058fae4bf0458a01d
congago <tnngocthinh4@gmail.com>
2016-11-29 10:03:46 +0700

    chapter 7 error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,9 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-  def create
-  	@user = User.new(user_params)
+ def create
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
